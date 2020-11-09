e1714c61928192b2b9082670f3e99c1c676394db
Ashu <ashuaman111@gmail.com>
2020-11-09 09:45:08 +0530

    uploaded all files from local

---
+++ test.c
@@ -0,0 +1,31 @@
+int x=1;
+int y=2;
+int z=x+y;
+float a=1.2;
+char b='a';
+//definitions
+
+
+
+
+for(int i=0;i<10;i++)
+{
+    if(i==5)
+    break;
+
+    continue;
+}
+
+while(i<20)
+{
+    i++;
+}
+/*     end of program              
+  
+  */
+
+
+ 
+
+
+
